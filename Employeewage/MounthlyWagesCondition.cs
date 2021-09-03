d58a7d81f6f2a982ecd88f1cbc8edf2982062076
varun <varunlad5@gmail.com>
2021-09-03 14:01:15 +0530

    [varun] User case 6 condition Statement

--- Employeewage/MounthlyWagesCondition.cs
+++ Employeewage/MounthlyWagesCondition.cs
@@ -8,38 +8,41 @@ namespace Employeewage
 {
     class attendence
     {
-           
+        
         public const int fulltime = 1;
         public const int Parttime = 2;
         static void Main(string[] arug)
         {
-            int EmployeeCheck;
-            int PerHourPay = 20, salary=0, TotalPay=0 , HoursPerDay;
+            int EmployeeCheck , HoursPerDay ,day=0 , Hours = 0;
+            int PerHourPay = 20, salary=0, TotalPay=0  ;
             Random random = new Random();
-            for (int day = 1; day < 20; day++)
+            while(Hours<=100 && day<=20 )
             {
+                
                 EmployeeCheck = random.Next(0, 3);
                 switch (EmployeeCheck)
                 {
                     case 1:
-                        Console.WriteLine("full Time  on day :"+day);
+                        Console.WriteLine("full Time  on day :" + day);
                         HoursPerDay = 8;
                         break;
                     case 2:
-                        Console.WriteLine("Part Time on day :" +day);
+                        Console.WriteLine("Part Time on day :" + day);
                         HoursPerDay = 4;
                         break;
                     default:
-                        Console.WriteLine("Absent on day :"+day);
+                        Console.WriteLine("Absent on day : "+day);
                         HoursPerDay = 0;
                         break;
                 }
+                Hours += HoursPerDay;
 
+                day++;
                 salary = PerHourPay * HoursPerDay;
                 Console.WriteLine("salary of Employee per day = " + salary);
                 TotalPay += salary;
             }
-            Console.WriteLine("Total Wage of Employee is :"+TotalPay);
+            Console.WriteLine("Total Wage of Employee is :{0} in days : {1}",TotalPay,day);
             Console.ReadLine();
         }
 
