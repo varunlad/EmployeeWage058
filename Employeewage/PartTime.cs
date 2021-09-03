2ec7211ef273a7c368734ab2b94b8f0f6bab3abb
varun <varunlad5@gmail.com>
2021-09-03 10:57:55 +0530

    [varun]User Case 3 PartTime is added

---
+++ Employeewage/PartTime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewage
+{
+    class attendence
+    {
+      
+        public const int fulltime = 1;
+        public const int Parttime = 2;
+        static void Main(string[] arug)
+        {   
+            int EmployeeCheck;
+            int PerHourPay = 20, salary, HoursPerDay;
+            Random random = new Random();
+            EmployeeCheck = random.Next(0, 3);
+            if (EmployeeCheck == fulltime)
+            {
+                HoursPerDay = 8;
+                Console.WriteLine("Employee work  full Time");
+            }
+            else if (EmployeeCheck == Parttime)
+            {
+                HoursPerDay = 8;
+                Console.WriteLine("Employee work  Part Time Time");
+            }
+            else
+            {
+                HoursPerDay = 0;
+                Console.WriteLine("Employee was Absent");
+            }
+            salary = PerHourPay * HoursPerDay;
+            Console.WriteLine("salary of Employee per day = " + salary);
+            Console.ReadLine();
+        }
+
+
+    }
+}
