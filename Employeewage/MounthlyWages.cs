8aaa333ec1f3c9004b7cf5cd735dfabd3f6891ed
varunlad <89180135+varunlad@users.noreply.github.com>
2021-09-04 18:32:57 +0530

    [varun] User Case 5 Mounthly Wages of Employee

--- Employeewage/MounthlyWages.cs
+++ Employeewage/MounthlyWages.cs
@@ -9,17 +9,17 @@ namespace Employeewage
     class attendence
     {
            
-        public const int fulltime = 1;
-        public const int Parttime = 2;
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
         static void Main(string[] arug)
         {
-            int EmployeeCheck;
-            int PerHourPay = 20, salary=0, TotalPay=0 , HoursPerDay;
+            int employeeCheck;
+            int perHourPay = 20, salary=0, totalPay=0 , hoursPerDay;
             Random random = new Random();
             for (int day = 1; day < 20; day++)
             {
-                EmployeeCheck = random.Next(0, 3);
-                switch (EmployeeCheck)
+                employeeCheck = random.Next(0, 3);
+                switch (employeeCheck)
                 {
                     case 1:
                         Console.WriteLine("full Time  on day :"+day);
@@ -35,11 +35,11 @@ namespace Employeewage
                         break;
                 }
 
-                salary = PerHourPay * HoursPerDay;
+                salary = perHourPay * hoursPerDay;
                 Console.WriteLine("salary of Employee per day = " + salary);
                 TotalPay += salary;
             }
-            Console.WriteLine("Total Wage of Employee is :"+TotalPay);
+            Console.WriteLine("Total Wage of Employee is :"+totalPay);
             Console.ReadLine();
         }
 
