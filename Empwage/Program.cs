3e906b1496d60ae12640c85b51d6633cecd4b51c
varun <varunlad5@gmail.com>
2021-09-16 20:29:30 +0530

    [varun] User case 9 Total Emp wage of Company

--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -8,48 +8,68 @@ namespace Empwage
 {
     class Program
     {
-
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        public static int EmpWage(string company, int PER_HOUR_PAY, int MAX_HOURS, int MAX_DAYS)
-        {
-            int EmployeeCheck, HoursPerDay, day = 0, Hours = 0;
-            int salary = 0, TotalPay = 0;
-            Random random = new Random();
-            while (Hours <= MAX_HOURS && day <= MAX_DAYS)
-            {
-
-                EmployeeCheck = random.Next(0, 3);
-                switch (EmployeeCheck)
-                {
-                    case 1:
-                        Console.WriteLine("full Time  on day :" + day);
-                        HoursPerDay = 8;
-                        break;
-                    case 2:
-                        Console.WriteLine("Part Time on day :" + day);
-                        HoursPerDay = 4;
-                        break;
-                    default:
-                        Console.WriteLine("Absent on day : " + day);
-                        HoursPerDay = 0;
-                        break;
-                }
-                Hours += HoursPerDay;
-                day++;
-                salary = PER_HOUR_PAY * HoursPerDay;
-                Console.WriteLine("salary of Employee per day = " + salary);
-                TotalPay += salary;
-            }
-            Console.WriteLine("Total Wage of Employee is :{0} in days : {1}", TotalPay, day);
-            return TotalPay;
-        }
+        
         static void Main(string[] arug)
         {
-            EmpWage("Dmart", 80, 50, 10);
-            EmpWage("Orion", 90, 60, 10);
-            EmpWage("Kmall", 60, 60, 15);
+            EmpWageBuilderObject Dmart = new EmpWageBuilderObject("Dmart", 20, 100, 20);
+            Dmart.ComputeEmpWage();
+            Console.WriteLine(Dmart.toString());
             Console.ReadLine();
+        }
+    }
+    public class EmpWageBuilderObject
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        private string company;
+        private int empRatePerHour;
+        private int noOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int maxHoursPerMonth, int noOfWorkingDays)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.noOfWorkingDays = noOfWorkingDays;
+        }
+           public void ComputeEmpWage()
+            { 
+            int empCheck,empHours, totalWorkingDay = 0, totalEmpHours = 0;
+            
+            Random random = new Random();
+                while (totalEmpHours <= this.maxHoursPerMonth && totalWorkingDay < this.noOfWorkingDays)
+                {
+                totalWorkingDay++;
+                empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case 1:
+                            Console.WriteLine("full Time  on day :" + totalWorkingDay);
+                        empHours = 8;
+                            break;
+                        case 2:
+                            Console.WriteLine("Part Time on day :" + totalWorkingDay);
+                        empHours = 4;
+                            break;
+                        default:
+                            Console.WriteLine("Absent on day : " + totalWorkingDay);
+                        empHours = 0;
+                            break;
+                    }
+                totalEmpHours += empHours;
+                
+                int salary = this.empRatePerHour * empHours;
+                    Console.WriteLine("salary of Employee per day = " + salary);
+                totalEmpWage += salary;
+                }
+            }
+        public string toString()
+        { 
+            return "Total Wage of Employee is :" +this.totalEmpWage +" in company : " +this.company;
+            
         }
     }
 }
