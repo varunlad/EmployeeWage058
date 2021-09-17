b63ec6723a88ad4ce6492aabf597cf205a7e9444
varun <varunlad5@gmail.com>
2021-09-17 23:26:14 +0530

    [varun] User case 11 using Interface approach

--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -8,68 +8,98 @@ namespace Empwage
 {
     class Program
     {
-        
+
         static void Main(string[] arug)
         {
-            EmpWageBuilderObject Dmart = new EmpWageBuilderObject("Dmart", 20, 100, 20);
-            Dmart.ComputeEmpWage();
-            Console.WriteLine(Dmart.toString());
+            EmpWageBuildertArray empWageBuilder = new EmpWageBuildertArray();
+            empWageBuilder.addCompanyEmpWage("Dmart",20,20,100);
+            empWageBuilder.addCompanyEmpWage("Kmall", 50, 20, 100);
+            empWageBuilder.computeEmpWage();
             Console.ReadLine();
         }
     }
-    public class EmpWageBuilderObject
+    public class CompanyEmpWage
     {
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        private string company;
-        private int empRatePerHour;
-        private int noOfWorkingDays;
-        private int maxHoursPerMonth;
-        private int totalEmpWage;
+        public string company;
+        public int empRatePerHour;
+        public int noOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
 
-
-        public EmpWageBuilderObject(string company, int empRatePerHour, int maxHoursPerMonth, int noOfWorkingDays)
+        public CompanyEmpWage(string company, int empRatePerHour, int maxHoursPerMonth, int noOfWorkingDays)
         {
             this.company = company;
             this.empRatePerHour = empRatePerHour;
             this.maxHoursPerMonth = maxHoursPerMonth;
             this.noOfWorkingDays = noOfWorkingDays;
         }
-           public void ComputeEmpWage()
-            { 
+    
+    public void setTotalEmpWage(int totalEmpWage)
+    {
+        this.totalEmpWage = totalEmpWage;
+    }
+    public string toString()
+    {
+        return "Total Wage of Employee is :" + this.totalEmpWage + " in company : " + this.company;
+
+    } 
+}
+
+    public class EmpWageBuildertArray
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] CompanyEmpWageArray;
+        public EmpWageBuildertArray()
+        {
+            this.CompanyEmpWageArray = new CompanyEmpWage[5];
+        }
+        public void addCompanyEmpWage( string company,int empRatePerHour,int numOfWorkingDays,int MaxHoursPerMonth)
+        {
+            CompanyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, MaxHoursPerMonth);
+            numOfCompany++;
+        }
+        public void computeEmpWage()
+        {
+            for (int i=0;i< numOfCompany;i++)
+            {
+                CompanyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(this.CompanyEmpWageArray[i]));
+                Console.WriteLine(this.CompanyEmpWageArray[i].tostring());
+            }
+        }
+           private int  ComputeEmpWage(CompanyEmpWage companyEmpWage)
+          { 
             int empCheck,empHours, totalWorkingDay = 0, totalEmpHours = 0;
             
             Random random = new Random();
-                while (totalEmpHours <= this.maxHoursPerMonth && totalWorkingDay < this.noOfWorkingDays)
-                {
+            while (totalEmpHours <= computeEmpWage.maxHoursPerMonth && totalWorkingDay < computeEmpWage.numOfWorkingDays)
+            {
                 totalWorkingDay++;
                 empCheck = random.Next(0, 3);
-                    switch (empCheck)
-                    {
-                        case 1:
-                            Console.WriteLine("full Time  on day :" + totalWorkingDay);
+                switch (empCheck)
+                {
+                    case 1:
+                        Console.WriteLine("full Time  on day :" + totalWorkingDay);
                         empHours = 8;
-                            break;
-                        case 2:
-                            Console.WriteLine("Part Time on day :" + totalWorkingDay);
+                        break;
+                    case 2:
+                        Console.WriteLine("Part Time on day :" + totalWorkingDay);
                         empHours = 4;
-                            break;
-                        default:
-                            Console.WriteLine("Absent on day : " + totalWorkingDay);
+                        break;
+                    default:
+                        Console.WriteLine("Absent on day : " + totalWorkingDay);
                         empHours = 0;
-                            break;
-                    }
-                totalEmpHours += empHours;
-                
-                int salary = this.empRatePerHour * empHours;
-                    Console.WriteLine("salary of Employee per day = " + salary);
-                totalEmpWage += salary;
+                        break;
                 }
+                totalEmpHours += empHours;
+                Console.WriteLine("Days "+totalWorkingDay +"Emp Hours :"+empHours);
             }
-        public string toString()
-        { 
-            return "Total Wage of Employee is :" +this.totalEmpWage +" in company : " +this.company;
-            
-        }
+                return totalEmpHours * companyEmpWage.empRatePerHour;
+                
+                
+               
+           }
+       
     }
 }
